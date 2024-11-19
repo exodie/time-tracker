43eaae06b433016bb07549e10013b43fac72bc80
IvanMitrofanov <83586607+exodie@users.noreply.github.com>
2024-11-19 10:36:15 +0400

    feat: add project back link into notes

--- src/widgets/notes/ui/ui.tsx
+++ src/widgets/notes/ui/ui.tsx
@@ -1,10 +1,17 @@
 import { NotesList } from './list'
+import { ArrowLeft } from 'lucide-react'
+
+import { Link } from 'react-router-dom'
 
 const PROJECT_NAME_MOCK = 'Project1'
 
 export const Notes = () => {
   return (
     <section className="flex flex-col my-4 gap-y-4">
+      <Link to={'/'} className="flex space-x-4 text-blue-600 hover:text-blue-500 font-medium w-fit">
+        <ArrowLeft /> Projects
+      </Link>
+
       <h1 className="font-mono text-2xl font-medium">Notes on {PROJECT_NAME_MOCK}</h1>
 
       <NotesList />
